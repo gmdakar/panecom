3eed7934aa6c6617463d39a2da900ea68e16e988
gmd <gmdakar@yahoo.fr>
2022-02-11 09:12:48 +0000

    new commmit

--- deployprofil.sh
+++ deployprofil.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+#
 echo "git add . && git commit -m "new commmit" && git push"
 git add . && git commit -m "new commmit" && git push
 
+#
 echo "drush -y cex && git status"
 drush -y cex && git status
