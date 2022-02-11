143b9bcd36cc2074c133d277bdb0118730f6f65a
gmd <gmdakar@yahoo.fr>
2022-02-11 09:11:30 +0000

    new commmit

--- deployprofil.sh
+++ deployprofil.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-# My first script
+echo "git add . && git commit -m "new commmit" && git push"
+git add . && git commit -m "new commmit" && git push
+
 echo "drush -y cex && git status"
 drush -y cex && git status
