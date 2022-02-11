9769cfe8090ec9a0ad5ebe808762b68b6a08e97d
gmd <gmdakar@yahoo.fr>
2022-02-11 09:14:49 +0000

    new commit

--- deployprofil.sh
+++ deployprofil.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-##
 echo "git add . && git commit -m "new commmit" && git push"
-git add . && git commit -m "new commmit" && git push
+git add . && git commit -m "new commit" && git push
 
-#
 echo "drush -y cex && git status"
 drush -y cex && git status
+
+echo "rm -rf modules/custom/mydefaultcontent/content && drush dcer node --folder=modules/custom/mydefaultcontent/content && drush dcer menu_link_content --folder=modules/custom/mydefaultcontent/content"
+rm -rf modules/custom/mydefaultcontent/content && drush dcer node --folder=modules/custom/mydefaultcontent/content && drush dcer menu_link_content --folder=modules/custom/mydefaultcontent/content
