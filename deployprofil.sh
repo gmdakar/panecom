c9ce122cf72264b7e40a55c96c72edbd00fb58a3
gmd <gmdakar@yahoo.fr>
2022-02-11 13:29:30 +0000

    new commit

--- deployprofil.sh
+++ deployprofil.sh
@@ -21,11 +21,11 @@ set -e
 echo "ls -al index.php"
 ls -al index.php
 
+echo "drush cr && drush -y cex && git status"
+drush cr && drush -y cex && git status
+
 echo "git add . && git commit -m "new commmit" && git push"
 git add . && git commit -m "new commit" && git push
-
-echo "drush -y cex && git status"
-drush -y cex && git status
 
 echo "rm -rf modules/custom/mydefaultcontent/content && drush dcer node --folder=modules/custom/mydefaultcontent/content && drush dcer menu_link_content --folder=modules/custom/mydefaultcontent/content"
 rm -rf modules/custom/mydefaultcontent/content && drush dcer node --folder=modules/custom/mydefaultcontent/content && drush dcer menu_link_content --folder=modules/custom/mydefaultcontent/content
