d2c80a979a2275209cd0941ad8765cfb25c1efa7
gmd <gmdakar@yahoo.fr>
2022-02-23 18:14:36 +0000

    new commmit

---
+++ sites/default/files/js/optimized/js_ETIR58oqhC5NZ-CedodNvdjO-Ty970-AMf2ifT7odCY.0Hgob-74cwWtr5KyMNzdPnyKivWAMwfUF8Z0bYSNj2o.js
@@ -0,0 +1,51 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function ($, Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.panecom_bstrp = {
+    attach: function (context, settings) {
+		
+		/*cacher le block notifications si on clique sur clear all de la liste des notifs*/
+		$('.clear-all-notification').click(function() {
+		  console.log("click on clear-all-notification");
+		  $("#block-notificationwidgetblock").hide();
+		});
+		
+		/*cacher le block notifications si on a 0 notif*/
+		$('.noti-store-msg').click(function() {
+		  console.log("click on one notification:", $("#notificationcount").attr('data-count'));
+		  if ($("#notificationcount").attr('data-count') == '1') /*it means it was the last currently visible*/
+			  $(this).closest("#block-notificationwidgetblock").hide();
+		});
+    }
+  };
+  
+  Drupal.behaviors.stickyNavbar = {
+        attach: function (context, settings) {
+
+            var navbar = $('.header');
+
+            if (navbar.length) {
+                var elmHeight = 25;
+                $(window).scroll(function() {
+                    var scrolltop = $(window).scrollTop();
+                    if (scrolltop > elmHeight) {
+                        if (!navbar.hasClass('sticky')) {
+                            navbar.addClass('sticky');
+                        }
+                    } else {
+                        navbar.removeClass('sticky');
+                    }
+                });
+            }
+
+        }
+
+  };
+
+})(jQuery, Drupal);
