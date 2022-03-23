87343a3781049aebd65b5eceebeef9b19d0b69e1
gmd <gmdakar@yahoo.fr>
2022-03-23 14:12:08 +0000

    new commit

--- profiles/panecom/panecom.post_update.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-/**
- * Install the Devel module.
- */
-function panecom_post_update_install_drush_empty_module2()
-{
-   // \Drupal::service('module_installer')->install(['default_content', 'mydefaultcontent']);
-}
