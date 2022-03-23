710c40a6cefad938c329073b618f675d5593248a
gmd <gmdakar@yahoo.fr>
2022-03-23 17:40:49 +0000

    new commit

--- deployprofil.sh
+++ deployprofil.sh
@@ -72,12 +72,15 @@ echo "drush -y config-set system.performance js.preprocess TRUE"
 drush -y config-set system.performance js.preprocess TRUE
 
 #les contenus existants 
-echo "drush -y en mydefaultcontent || true"
-drush -y en mydefaultcontent || true
+#echo "drush -y pmu mydefaultcontent || true && drush -y en mydefaultcontent || true"
+#drush -y en mydefaultcontent || true
 
 #car le module layout builder semble pas bien installer certaines configs layouts lors de la phase d'installation
 echo "drush -y cex ; drush -y cim ; drush cr"
 drush -y cex ; drush -y cim ; drush cr
+
+echo "drush -y en mydefaultcontent || true"
+drush -y en mydefaultcontent || true
 
 echo "............................"
 echo "***  PROFIL INSTALLED  *** "
