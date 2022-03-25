a9d9b14e776b4743119bd894d58086d5478ba78f
gmd <gmdakar@yahoo.fr>
2022-03-25 12:00:50 +0000

    new commit

--- deployprofil.sh
+++ deployprofil.sh
@@ -45,6 +45,10 @@ chmod -R 775 ../panecomdistr/ || true && cd ../../htdocs && rm -rf panecomdistr/
 
 
 ############### PHASE D'INSTALLATION DU PROFIL DEPLOYEE sur l'instance de démo "panecomdistr"  ###############################
+echo "####"
+echo "#### CAUTION : the module mydefaultcontent needs to be marked activated by default by all the ways ####"
+echo "####"
+
 echo "cd panecomdistr"
 cd panecomdistr
 
@@ -66,10 +70,12 @@ composer require drush/drush:10.x -W --no-interaction
 echo "sed  's/standard/panecom/g' sites/*/*/*/*/core.extension.yml > delete.txt"
 sed  's/standard/panecom/g' sites/*/*/*/*/core.extension.yml > delete.txt
 
+echo "mv delete.txt sites/*/*/*/*/core.extension.yml"
+mv delete.txt sites/*/*/*/*/core.extension.yml
+
 #grep -q "mydefaultcontent" delete.txt; [ $? -eq 0 ] && echo "module mydefaultcontent is already activated" || sed '10 a\  mydefaultcontent: 0'  delete.txt > delete2.txt && mv delete2.txt sites/*/*/*/*/core.extension.yml
 #echo "sed '10 a\  mydefaultcontent: 0' delete.txt > delete2.txt && mv delete2.txt sites/*/*/*/*/core.extension.yml"
 #sed '10 a\  mydefaultcontent: 0' delete.txt > delete2.txt && mv delete2.txt sites/*/*/*/*/core.extension.yml
-echo "#### CAUTION : the module mydefaultcontent needs to be marked activated by default by all the ways ####"
 
 echo "rm delete.txt || true"
 rm delete.txt || true
