a45c515fffb8f57db4ee4dc3d2e3f22e4a803b2d
gmd <gmdakar@yahoo.fr>
2022-04-01 11:50:24 +0000

    new commit

--- deployprofil.sh
+++ deployprofil.sh
@@ -85,6 +85,8 @@ mv delete.txt sites/*/*/*/*/core.extension.yml
 echo "rm delete.txt || true"
 rm delete.txt || true
 
+drush sql-drop --yes 
+
 #echo "drush sql-drop --yes || true && drush -y site-install --existing-config --db-url=mysql://db-panecomdistr:xxxxxx@cloudpanel.digissol.pro:3306/db-panecomdistr --account-name=admin --account-pass=Passer@123 --site-name=PANECOM --site-mail=test@testpanecom.com"
 #drush sql-drop --yes || true && drush -y site-install --existing-config --db-url=mysql://db-panecomdistr:99nTm8u4ZC@cloudpanel.digissol.pro:3306/db-panecomdistr --account-name=admin --account-pass=Passer@123 --site-name=PANECOM --site-mail=test@testpanecom.com
 
