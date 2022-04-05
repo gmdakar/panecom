de4b3aa2e874a84b2c1d80933f8c2a6bea798728
gmd <gmdakar@yahoo.fr>
2022-04-05 09:01:55 +0000

    new commit

--- deployprofil.sh
+++ deployprofil.sh
@@ -92,8 +92,8 @@ echo "drush sql-drop --yes || true && drush -y site-install --existing-config --
 drush sql-drop --yes || true && drush -y site-install --existing-config --db-url=mysql://db-panecomdistr:99nTm8u4ZC@cloudpanel.digissol.pro:3306/db-panecomdistr --account-name=admin --account-pass=Passer@123 --site-name=PANECOM --site-mail=test@testpanecom.com
 
 #car le module layout builder semble pas bien installer certaines configs layouts lors de la phase d'installation
-echo "drush -y cim"
-drush -y cim
+#echo "drush -y cim"
+#drush -y cim
 
 echo "drush -y config-set system.performance css.preprocess TRUE"
 drush -y config-set system.performance css.preprocess TRUE
