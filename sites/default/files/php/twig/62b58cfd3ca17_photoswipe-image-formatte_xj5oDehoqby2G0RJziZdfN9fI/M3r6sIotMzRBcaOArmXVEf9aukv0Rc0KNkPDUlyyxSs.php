f755b024cc34ffdb934c45683a4f7c9dc2517880
gmd <gmdakar@yahoo.fr>
2022-06-24 12:28:12 +0000

    new commit

---
+++ sites/default/files/php/twig/62b58cfd3ca17_photoswipe-image-formatte_xj5oDehoqby2G0RJziZdfN9fI/M3r6sIotMzRBcaOArmXVEf9aukv0Rc0KNkPDUlyyxSs.php
@@ -0,0 +1,112 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* modules/contrib/photoswipe/templates/photoswipe-image-formatter.html.twig */
+class __TwigTemplate_ec33c088540a1fb0d0cf087cfd45ffcef74c229700496e07ffcae6800cd8ded5 extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $this->checkSecurity();
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 16
+        echo "<a href=\"";
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["path"] ?? null), 16, $this->source), "html", null, true);
+        echo "\"";
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["attributes"] ?? null), 16, $this->source), "html", null, true);
+        echo ">";
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["image"] ?? null), 16, $this->source), "html", null, true);
+        echo "</a>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "modules/contrib/photoswipe/templates/photoswipe-image-formatter.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  39 => 16,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("{#
+/**
+ * @file
+ * Returns HTML for an image using a specific Photoswipe image style.
+ *
+ * Available variables:
+ * - image: image item.
+ * - path: The path of the image that should be displayed in Photoswipe.
+ * - attributes: Link attributes.
+ * - item: An ImageItem object.
+ * - display_settings: optional image styles.
+ *
+ * @ingroup themeable
+ */
+#}
+<a href=\"{{ path }}\"{{ attributes }}>{{ image }}</a>
+", "modules/contrib/photoswipe/templates/photoswipe-image-formatter.html.twig", "/home/cloudpanel/htdocs/devpanecom/modules/contrib/photoswipe/templates/photoswipe-image-formatter.html.twig");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = array();
+        static $filters = array("escape" => 16);
+        static $functions = array();
+
+        try {
+            $this->sandbox->checkSecurity(
+                [],
+                ['escape'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
