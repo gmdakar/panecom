c51fc72fb438b7ee13504c7974f39b1de787d2bc
mmdakar <gmdakar@yahoo.fr>
2022-08-12 22:33:11 +0000

    commit for patch

--- themes/custom/panecom_bstrp/js/global.js
+++ themes/custom/panecom_bstrp/js/global.js
@@ -6,6 +6,37 @@
 (function ($, Drupal) {
 
   'use strict';
+  
+  Drupal.behaviors.panecom_bstrp_actualites_quoi2_9 = {
+        attach: function (context, settings) {								
+								
+			//console.log( $('.block-views-blockles--block-2 .owl-stage > div.active').index());
+			//var prev_actu = current_actu.next().children().addBack().text();
+			//var next_actu = current_actu.next().children(".views-field-nid .field-content").andSelf().text();
+			//console.log(prev_actu);
+			//console.log(next_actu);
+			setTimeout(function() { 
+				var owl = $('.owl-carousel');
+				owl.owlCarousel(); 
+			
+				owl.on('changed.owl.carousel', function(e) {
+				    
+					//var current_actu = $('.block-views-blockles--block-2 .owl-stage > div.active').find(".views-field-nid .field-content").text();
+					var obj_prev_actu = $('.block-views-blockles--block-2 .owl-stage > div.active').prev();
+					var obj_next_actu = $('.block-views-blockles--block-2 .owl-stage > div.active').next();	
+					var prev_actu_small_img = obj_prev_actu.find(".views-field-field-image-1 .small-img img").attr("src");
+					var next_actu_small_img = obj_next_actu.find(".views-field-field-image-1 .small-img img").attr("src");
+					$(".block-views-blockles--block-2 .owl-stage > div.active").append("<style>.block-views-blockles--block-2 .owl-stage > div.active::before{background-image:url('"+prev_actu_small_img+"') !important; visibility: initial;");
+					$(".block-views-blockles--block-2 .owl-stage > div.active").append("<style>.block-views-blockles--block-2 .owl-stage > div.active::after{background-image:url('"+next_actu_small_img+"') !important; visibility: initial;");
+								
+					//console.log("prev_actu:" + prev_actu_small_img + " - current_actu:" + current_actu + " - next_actu:" + next_actu_small_img);
+					
+				 })
+			   
+			}, 500);
+			
+        }
+  };
 
   Drupal.behaviors.panecom_bstrp_notificationwidget = {
     attach: function (context, settings) {
@@ -128,42 +159,6 @@
         }
   };
   
-    Drupal.behaviors.panecom_bstrp_actualites_quoi2_9 = {
-        attach: function (context, settings) {
-			console.log("aaaaaaa");
-            var current_actu = $('.block-views-blockles--block-2 .owl-stage > div.active');
-			console.log( $('.block-views-blockles--block-2 .owl-stage > div.active').index());
-			//var prev_actu = current_actu.next().children().addBack().text();
-			//var next_actu = current_actu.next().children(".views-field-nid .field-content").andSelf().text();
-			//console.log(prev_actu);
-			//console.log(next_actu);
-			setTimeout(function() { 
-       
-	    var owl = $('.owl-carousel');
-     owl.owlCarousel(); 
-	
-     owl.on('changed.owl.carousel', function(e) {
-		
-		var current_actu = $('.block-views-blockles--block-2 .owl-stage > div.active').find(".views-field-nid .field-content").text();       
-	    var prev_actu = $('.block-views-blockles--block-2 .owl-stage > div.active').prev().find(".views-field-nid .field-content").text();
-	    var next_actu = $('.block-views-blockles--block-2 .owl-stage > div.active').next().find(".views-field-nid .field-content").text();
-		
-		console.log("prev_actu:" + prev_actu + " - current_actu:" + current_actu + " - next_actu:" + next_actu);
-		
-	   //$(this) refers to active content
-       //SELECT img TAG TO CHANGE THE src ATTRIBUTE
-       /*$(this).parents('.img-parent')
-              .find('img') 
-              .attr('src',`img${e.page.index}.svg`)*/
-     })
-	   
-    }, 1000);
-			
-	
-			
-
-        }
-
-  };
+    
 
 })(jQuery, Drupal);
